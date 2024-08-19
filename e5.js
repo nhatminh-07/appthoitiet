943aed09a41cb03b8bee2308dd17d9571a0b1815
nhatminh-07 <tranngocnhatminh07@gmail.com>
2024-08-19 13:42:37 +0700

    update

---
+++ e5.js
@@ -0,0 +1,14 @@
+// 1
+
+let a1 = Array()
+
+// 2
+let a2 = [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(a2)
+
+// 3
+console.log(a2.length)
+
+// 4
+console.log(a2[0], a2[5], a2[a2.length-1])
+
---
+++ e7.js
@@ -0,0 +1,19 @@
+// function without parameter,  a function which make a number square
+function square() {
+    let num = 2
+    let sq = num * num
+    console.log(sq)
+  }
+  
+  square() // 4
+  
+  // function without parameter
+  function addTwoNumbers() {
+    let numOne = 10
+    let numTwo = 20
+    let sum = numOne + numTwo
+  
+    console.log(sum)
+  }
+  
+  addTwoNumbers() // a function has to be called by its name to be executed 
